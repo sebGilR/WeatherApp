4a28b2eb06e90ae38d7ef8367ed7473a23ac596f
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-06-12 21:46:14 -0500

    Add elements and results are now displayed

--- src/ui.js
+++ src/ui.js
@@ -1,10 +1,46 @@
 
 const searchBox = document.getElementById('search');
 const submitBtn = document.getElementById('submit');
+const locationP = document.getElementById('location');
+const tempP = document.getElementById('temp');
+const flikeP = document.getElementById('flike');
+const weatherP = document.getElementById('weather');
+const alertP = document.getElementById('alert');
+
+const displayGif = (url) => {
+  const img = document.getElementById('gif');
+  img.src = url;
+  img.classList.remove('hidden');
+};
+
+const getGif = async (weather) => {
+  const response = await fetch(`http://api.giphy.com/v1/gifs/search?q=${weather}&limit=1&api_key=hZA0UJe9PBW5av8TZ6DSWJPnGAQ5XIdO`, { mode: 'cors' });
+  const result = await response.json();
+  if (result.cod !== '400' || result.cod !== '404') {
+    displayGif(result.data[0].images.original.url);
+  }
+};
+
+const showData = () => {
+  if (localStorage.weather) {
+    alertP.classList.add('hidden');
+    const data = JSON.parse(localStorage.weather);
+    getGif(data.weather);
+    locationP.innerText = `Location: ${data.country}, ${data.name}`;
+    weatherP.innerText = `Condition: ${data.description}`;
+    tempP.innerText = `Temperature: ${data.temp}°`;
+    flikeP.innerText = `Feeling like: ${data.flike}°`;
+    flikeP.parentNode.classList.remove('hidden');
+  } else {
+    flikeP.parentNode.classList.add('hidden');
+    alertP.innerText = 'Invalid City provided';
+    alertP.classList.remove('hidden');
+  }
+};
 
 const setListeners = (getWeather, city, units) => {
   submitBtn.addEventListener('click', () => {
-    getWeather(city, units);
+    getWeather(city, units, showData);
   }, false);
 
   searchBox.addEventListener('keypress', (e) => {
--- src/weather.js
+++ src/weather.js
@@ -1,14 +1,32 @@
-const usefulData = (data) => {
+const saveData = (data) => {
   if (data.cod === '400' || data.cod === '404') {
-    console.log('Weather not found for city provided');
-  } else {
-    console.log(data);
+    localStorage.clear();
+    return 'Weather not found for city provided';
   }
+  const {
+    main, name, sys, weather, wind,
+  } = data;
+  const filtered = {
+    weather: weather[0].description,
+    description: weather[0].main,
+    name,
+    temp: main.temp,
+    flike: main.feels_like,
+    humidity: main.humidity,
+    country: sys.country,
+    windSpeed: wind.speed,
+    windDirection: wind.deg,
+  };
+
+  return localStorage.setItem('weather', JSON.stringify(filtered));
 };
 
-const getWeather = async (city, units) => {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city()}&APPID=17bfcd723e4a10d65e366ef5bc79951f&units=${units()}`, { mode: 'cors' });
-  usefulData(await response.json());
+const getWeather = async (city, units, action) => {
+  const base = 'http://api.openweathermap.org/data/2.5/weather';
+  const key = '17bfcd723e4a10d65e366ef5bc79951f';
+  const response = await fetch(`${base}?q=${city()}&APPID=${key}&units=${units()}`, { mode: 'cors' });
+  saveData(await response.json());
+  action();
 };
 
 export default getWeather;
